67c1e0a7932217be402ee4b0ebd65e13339e0698
Farzan Nadeem <mfnadeem@edu.uwaterloo.ca>
2021-03-23 16:48:56 -0400

    code cleanup

--- public/index.js
+++ public/index.js
@@ -8,46 +8,46 @@ var savedContainer = null;
 var ip = ""
 var ipKey = "lanIP"
 
-window.onload = init()  
+window.onload = init()
 
-async function init() { 
+async function init() {
     savedContainer = document.getElementById("savedColorsContainer")
-    while(savedContainer == null) { 
+    while (savedContainer == null) {
         savedContainer = document.getElementById("savedColorsContainer")
         await sleep(50)
     }
 
-    
+
     ip = getCookie(ipKey)
     document.getElementById("ip").value = ip;
-    
-    savedColors = getCookie(savedColorsKey) 
-    if(savedColors.length < 1 ) return
-    
+
+    savedColors = getCookie(savedColorsKey)
+    if (savedColors.length < 1) return
+
     savedColorsArray = savedColors.split(",")
-    savedColorsNum = savedColorsArray.length;  
-    savedColorsArray.forEach(color => { 
+    savedColorsNum = savedColorsArray.length;
+    savedColorsArray.forEach(color => {
         createSavedColorButton(color)
-    }) 
+    })
 }
 
 // Saves the ip to the local variable and in the cookies
-function updateIP(e) { 
+function updateIP(e) {
     ip = e
 
     var d = new Date();
-    d.setTime(d.getTime() + (10*365*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString(); 
+    d.setTime(d.getTime() + (10 * 365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
 
     // Append to the custom color json and then set the cookie again to update it 
-    document.cookie = ipKey+"="+ip+";"+expires
+    document.cookie = ipKey + "=" + ip + ";" + expires
 }
 
 // We're going to be using a single click handler for the RGB up/down buttons
 // However, it'll send a distinct request for which hue is getting updated
 // The arduino code is quite verbose and simplistic, so I figured this would be the best way to minimze coding, on at least one end
 // e is the event object. It tells us which button was clicked via the ID of the button clicked
-function hue_change(e) { 
+function hue_change(e) {
     // Send this to controller
 
 }
@@ -56,7 +56,7 @@ function hue_change(e) {
 // Logically it is the same as the above
 // Could they have been combined? Definitely
 // Separated for logic and readability 
-function bright_change(e) { 
+function bright_change(e) {
     // Send this to controller
 
 }
@@ -65,7 +65,7 @@ function bright_change(e) {
 // The actual code for the different modes is stored on the controller
 // Here, we send the mode that we want to start on the controller as a "parameter"
 // The parameter is read and acted on from the controller side
-function mode(e) { 
+function mode(e) {
     // Send this to controller
 
 }
@@ -74,12 +74,12 @@ function mode(e) {
 // This will be intermittent until I figure out the rate that I can send data to the controller
 // This is going to be the blocking boolean
 var inputChangeBlocker = false
-function custom_color_picker_input(hex) { 
-    if(inputChangeBlocker)  { return; }
+function custom_color_picker_input(hex) {
+    if (inputChangeBlocker) { return; }
     inputChangeBlocker = true;
     var rgb = hexToRgb(hex)
     console.log(`Running the input function ${rgb}`)
-    
+
     // send to the controller
 
     // Reset the blocker after some ms, which will open the path for more sends to the controller
@@ -87,12 +87,12 @@ function custom_color_picker_input(hex) {
         inputChangeBlocker = false
     }, 100);
 }
- 
+
 // This is going to be the custom colour picker's way of sending the custom color
 // The request would send the "R" "G" and "B" values as parameters to the controller
 // The parameters are going to be read and then acted on from the controller side
 // This will be the event that fires at the end of the changing
-function custom_color_picker_change(hex) { 
+function custom_color_picker_change(hex) {
     var rgb = hexToRgb(hex)
     console.log(rgb)
 
@@ -100,37 +100,37 @@ function custom_color_picker_change(hex) {
 }
 
 // This will be the function that saves the value of the custom color that was just made to the browser cookies
-function custom_color_save() { 
-    var value = document.getElementById("colorPicker").value 
- 
-    if(savedColors.length < 1) {
+function custom_color_save() {
+    var value = document.getElementById("colorPicker").value
+
+    if (savedColors.length < 1) {
         savedColors = value
-    } else { 
+    } else {
         savedColors += "," + value
-    } 
+    }
 
     var d = new Date();
-    d.setTime(d.getTime() + (10*365*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString(); 
+    d.setTime(d.getTime() + (10 * 365 * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
 
     // Append to the custom color json and then set the cookie again to update it 
-    document.cookie = savedColorsKey+"="+savedColors+";"+expires
+    document.cookie = savedColorsKey + "=" + savedColors + ";" + expires
 
     // Add the color to the ui 
     createSavedColorButton(value)
 }
 
 // Opposite of above, clears everything
-function clearSavedColors(e) { 
+function clearSavedColors(e) {
 
-    if (confirm("Are you sure? There is no way to retrieve these items once lost")) { 
+    if (confirm("Are you sure? There is no way to retrieve these items once lost")) {
         var d = new Date();
-        d.setTime(d.getTime() - (10*24*60*1000));
-        var expires = "expires="+ d.toUTCString(); 
-    
+        d.setTime(d.getTime() - (10 * 24 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+
         // Append to the custom color json and then set the cookie again to update it 
-        document.cookie = savedColorsKey+"="+savedColors+";"+expires
-    
+        document.cookie = savedColorsKey + "=" + savedColors + ";" + expires
+
         savedContainer.innerHTML = ""
     }
 }
@@ -138,7 +138,7 @@ function clearSavedColors(e) {
 // This will send a custom color to the controller to be displayed
 // This color will be sent as the "R", "G", and "B" parameter
 // The controller will read the parameters and then display it on the LED
-function custom_color(e) { 
+function custom_color(e) {
     console.log(e)
     var rgb = hexToRgb(e.value)
     // Send this to controller
@@ -158,7 +158,7 @@ function hexToRgb(hex) {
     } : null;
 }
 
-function createSavedColorButton(color) { 
+function createSavedColorButton(color) {
     var classes = "btn-floating btn-large waves-effect waves-light pressable"
     var tag = `<button value="${color}" onclick="custom_color(this.value)" style="background:${color}" class="${classes}"></button>`
     savedContainer.innerHTML += tag
